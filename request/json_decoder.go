e533771d3b0a53ae5fdd01f41897c381faec597b
nhoway <6579400+nhoway@users.noreply.github.com>
2022-01-30 12:50:58 +0000

    updated json decoder for empty body

--- request/json_decoder.go
+++ request/json_decoder.go
@@ -10,5 +10,11 @@ type JsonBodyDecoder struct{}
 func (d *JsonBodyDecoder) DecodeBody(body io.ReadCloser, target interface{}, obj interface{}) error {
 	// otherwise use the default decoder
 	decoder := json.NewDecoder(body)
-	return decoder.Decode(&target)
+	err := decoder.Decode(&target)
+
+	// do not return an error on empty body
+	if err != nil && err.Error() == "EOF" {
+		return nil
+	}
+	return err
 }
